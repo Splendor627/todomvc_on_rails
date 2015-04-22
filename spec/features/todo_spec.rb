8688140bc03e55cf141c26472b85d1cccc1d1f58
Kenny Clark <splendor627@gmail.com>
2015-04-22 17:53:51 -0400

    Verified the rest of the text was located on homepage

--- spec/features/todo_spec.rb
+++ spec/features/todo_spec.rb
@@ -1,17 +1,21 @@
 require 'rails_helper'
 
-feature "The homepage" do
-  background do
-    create(:todo, title: "buy milk & eggs")
+feature "Todos" do
+  scenario "that are incomplet are marked as such" do
+    create(:todo, title: "buy milk & eggs", completed: false)
+
+    visit "/"
+    expect(page).to have_selector("li label", text: "buy milk & eggs")
+    expect(page).to_not have_selector("li.completed label", text: "buy milk & eggs")
   end
 
-  scenario "should show my Todos" do
-    visit "/"
-    expect(page).to have_content("buy milk & eggs")
-  end
+  scenario "that are completed are marked as such" do
+    create(:todo, title: "buy milk & eggs", completed: true)
 
-  scenario "should credit the author" do
     visit "/"
-    expect(page).to have_content("Created by Matt De Leon")
+    # since there's no good way to assert specific styles (such as
+    # text-decoration: line-through) we have to go this route and trust the css
+    # works
+    expect(page).to have_selector("li.completed label", text: "buy milk & eggs")
   end
 end
